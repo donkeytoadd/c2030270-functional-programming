992ca076e8c4ba19f51cc9148299ae4b9a6816e3
donkeytoadd <jordan.aaron.hanson@gmail.com>
2026-01-05 17:21:49 +0000

    refactor problem 2 to incorporate more specs. added more validation of user inputs, added a default path for better user experience. added comments.

--- c2030270-assessment/src/c2030270_assessment/problem2.clj
+++ c2030270-assessment/src/c2030270_assessment/problem2.clj
@@ -2,16 +2,56 @@
   (:require [clojure.spec.alpha :as s]
             [clojure.string :as str]))
 
+(def default-student-list ["Alice" "Bob" "Charlie" "David" "Eve" "Fred" "Ginny" "Harriet" "Ileana" "Joseph" "Kincaid" "Larry"])
+(def default-row1 "VRCGVVRVCGGCCGVRGCVCGCGV")
+(def default-row2 "VRCCCGCRRGVCGCRVVCVGCGCV")
+
+(def plant-codes #{\G \C \R \V})
+(def character->plant {\G "grass" \C "clover" \R "radishes" \V "violets"})
+
+;valid student name consists of a non blank string
 (s/def ::student-name (s/and string? #(not (str/blank? %))))
-(s/def ::students (s/coll-of ::student-name :kind vector? :min-count 1))
+
+;a garden row must be a string that is made up of valid plant codes
+(s/def ::row (s/and string? (partial every? plant-codes)))
+
+(defrecord Garden [row1 row2])
+
+(s/def ::garden-record
+  (s/and
+   #(instance? Garden %)
+   ;checks the rows of the garden against the row specification to validate them
+   ;also checks if rows are of equal length
+   (fn [garden]
+     (and (s/valid? ::row (:row1 garden))
+          (s/valid? ::row (:row2 garden))
+          (= (count (:row1 garden))
+             (count (:row2 garden)))))))
+
+;a wrapper for the defrecord Garden constructor above. adds validation by checking against the garden specifciation
+(defn ->garden [row1 row2]
+  (let [garden (->Garden row1 row2)]
+    (when-not (s/valid? ::garden-record garden)
+      (throw (ex-info "Invalid garden rows" {:row1 row1 :row2 row2})))
+    garden))
+
+(defn generate-garden [amount-of-students]
+  (let [random-plant #(rand-nth (vec plant-codes))
+        row-length (* 2 amount-of-students)
+        create-row #(apply str (repeatedly row-length random-plant))]
+    (->garden (create-row) (create-row))))
 
 (defn generate-students []
-  (println "Input the number of students you want: ")
-  (flush)
-  (let [amount-of-students (try (Integer/parseInt (read-line))
-                                (catch Exception ex
-                                  (println "Error:" (.getMessage ex))
-                                  (generate-students)))]
+  (let [amount-of-students (loop []
+                             (println "Enter the amount of students you want in your list")
+                             (flush)
+                             (let [input (read-line)]
+                               (if (re-matches #"\d+" input)
+                                 (let [n (Integer/parseInt input)]
+                                   (if (pos? n)
+                                     n
+                                     (do (println "Student list must be at least 1") (recur))))
+                                 (do (println "Invalid input, please enter a whole number") (recur)))))]
     (loop [count 0 students []]
       (if (= count amount-of-students)
         students
@@ -23,34 +63,52 @@
               (recur (inc count) (conj students name))
               (do (println "Invalid name, please try again") (recur count students)))))))))
 
-;load from config file or database type structure, makes it better
-(def plant-codes [\G \C \R \V])
+(defn normalise-students [students]
+  ;removes any duplicate names and sorts alphabetically, then ensures a vector is returned
+  (->> students distinct sort vec))
 
-(defn generate-garden [amount-of-students]
-  (let [random-plant #(rand-nth plant-codes)
-        row-length (* 2 amount-of-students)
-        create-row #(apply str (repeatedly row-length random-plant))]
-    [(create-row) (create-row)]))
-
-(def map-character-to-plant {\G "grass" \C "clover" \R "radishes" \V "violets"})
+(defn get-student-index [sorted-students student]
+  (let [index (.indexOf sorted-students student)]
+    (when (= -1 index)
+      (throw (ex-info "Student not found" {:student student})))
+    index))
 
 (defn get-student-plants [garden students student]
-  (let [[row1 row2] garden
-        sorted-students (sort students)
-        index (.indexOf sorted-students student)
+  (let [index (get-student-index students student)
         start (* index 2)
-        end (+ start 2)
-        student-cups (concat (subs row1 start end) (subs row2 start end))]
-    (map map-character-to-plant student-cups)))
+        end   (+ start 2)
+        row1  (:row1 garden)
+        row2  (:row2 garden)
+        cups  (str (subs row1 start end) (subs row2 start end))]
+    (mapv character->plant cups)))
 
 (defn main []
-  (let [students (generate-students)
-        garden (generate-garden (count students))
-        [row1 row2] garden]
-    (println "\nStudents: " students)
-    (println "\nGarden:\n" row1 "\n" row2)
-    (println "\nEnter a student's name to see their plants:")
-    (let [input (read-line)]
-      (println input "has plants:" (vec (get-student-plants garden students input))))))
+  (println "Choose a mode:\n1: Default(student list & garden same as brief)\n2: Interactive (make your own student list & random garden generation)")
+  (flush)
+  (let [choice (str/trim (read-line))
+        use-default (= "1" choice)
+        students (if use-default default-student-list (normalise-students (generate-students)))
+        garden (if use-default (->garden default-row1 default-row2) (generate-garden (count students)))]
+
+    (println "\nStudents:")
+    (println students)
+
+    (println "\nGarden:")
+    (println (:row1 garden))
+    (println (:row2 garden))
+
+    (println "\n Type 'exit' to quit")
+
+    ;loops until user exits, checks inputted name and returns error if not found in list
+    (loop []
+      (println "\nEnter a student's name to see their plants:")
+      (flush)
+      (let [input (read-line)]
+        (when-not (= "exit" input)
+          (try
+            (println input "has plants:" (get-student-plants garden students input))
+            (catch Exception ex
+              (println "\nError:" (.getMessage ex))))
+          (recur))))))
 
 (main)
--- c2030270-assessment/test/c2030270_assessment/problem2_test.clj
+++ c2030270-assessment/test/c2030270_assessment/problem2_test.clj
@@ -1,28 +1,57 @@
 (ns c2030270-assessment.problem2-test
   (:require [clojure.test :refer :all]
-            [c2030270-assessment.problem2 :refer :all]))
+            [c2030270-assessment.problem2 :refer :all]
+            [clojure.spec.alpha :as s]))
+
+(def example-row1 "VRCGVVRVCGGCCGVRGCVCGCGV")
+(def example-row2 "VRCCCGCRRGVCGCRVVCVGCGCV")
+
+(def example-students
+  ["Alice" "Bob" "Charlie" "David" "Eve" "Fred"
+   "Ginny" "Harriet" "Ileana" "Joseph" "Kincaid" "Larry"])
+
+(def unordered-students
+  ["Larry" "Alice" "Ileana" "Fred" "Charlie" "Bob"
+   "David" "Eve" "Larry" "Kincaid" "Ginny" "Joseph"])
 
 (deftest get-student-plants-test
-  (let [garden ["VRCGVVRVCGGCCGVRGCVCGCGV" "VRCCCGCRRGVCGCRVVCVGCGCV"]
-        students ["Alice" "Bob" "Charlie" "David" "Eve" "Fred" "Ginny" "Harriet" "Ileana" "Joseph" "Kincaid" "Larry"]
-        unordered-students ["Larry" "Alice" "Ileana" "Fred" "Charlie" "Bob" "David" "Eve" "Larry" "Kincaid" "Ginny" "Joseph"]]
+  (let [garden (->garden example-row1 example-row2)]
 
     (testing "Returns correct plants for Alice"
-      (is (= (get-student-plants garden students "Alice") '("violets" "radishes" "violets" "radishes"))))
+      (is (= (get-student-plants garden example-students "Alice") ["violets" "radishes" "violets" "radishes"])))
 
     (testing "Returns correct plants for Bob"
-      (is (= (get-student-plants garden students "Bob") '("clover" "grass" "clover" "clover"))))
+      (is (= (get-student-plants garden example-students "Bob") ["clover" "grass" "clover" "clover"])))
 
     (testing "Returns correct plants with unordered students list"
-      (is (= (get-student-plants garden unordered-students "Alice") '("violets" "radishes" "violets" "radishes"))))))
+      ;get-student-plants expects a sorted list as the input, this is done in the normalise-students function in the solution
+      (let [sorted-students (sort unordered-students)]
+        (is (= (get-student-plants garden sorted-students "Alice") ["violets" "radishes" "violets" "radishes"]))))))
 
 (deftest map-character-to-plant-test
   (testing "Maps garden letters to correct plant names"
-    (is (= (map map-character-to-plant [\V \R \G \C]) '("violets" "radishes" "grass" "clover")))))
+    (is (= (mapv character->plant  [\V \R \G \C]) ["violets" "radishes" "grass" "clover"]))))
 
 (deftest generate-garden-test
   (testing "Generates a garden with correct length of row for given amount of students"
     (let [amount-of-students 5 garden (generate-garden amount-of-students)]
-      (is (= 2 (count garden)))
-      (is (= (* 2 amount-of-students) (count (first garden))))
-      (is (= (* 2 amount-of-students) (count (second garden)))))))
+      (is (s/valid? :c2030270-assessment.problem2/garden-record garden))
+      (is (= (* 2 amount-of-students) (count (:row1 garden))))
+      (is (= (* 2 amount-of-students) (count (:row2 garden)))))))
+
+(deftest garden-constructor-test
+  (testing "Valid garden record"
+    (is (s/valid? :c2030270-assessment.problem2/garden-record (->garden example-row1 example-row2))))
+
+  (testing "Incorrect garden row length throws exception"
+    (is (thrown? clojure.lang.ExceptionInfo (->garden "VR" "VRC"))))
+
+  (testing "Invalid characters for plants throws exception"
+    (is (thrown? clojure.lang.ExceptionInfo (->garden "XY" "VR")))))
+
+(deftest get-student-index-test
+  (testing "Returns correct index for inputted name"
+    (is (= 0 (get-student-index default-student-list "Alice"))))
+
+  (testing "Throws exception when name not found"
+    (is (thrown? clojure.lang.ExceptionInfo (get-student-index default-student-list "Bartholomew")))))
